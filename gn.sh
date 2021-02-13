07eaab4b1d256fe9f850a8dc3658f20c00794062
AnonxCode <78537900+Xclowns9@users.noreply.github.com>
2021-02-13 19:42:06 +0700

    Add files via upload

---
+++ gn.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+echo $white " "
+figlet -f slant "Generator ig"
+echo $cyan"==========================================================="
+echo $white"                   Coded By AnonxCode"
+echo $white"                Generator Link Instagram"
+echo $cyan"==========================================================="
+
+echo $white "
+Name Your Instagram"
+read -p "
+==> " l
+echo "Wait.."
+sleep 3
+echo $red "Hasil : https://instagram.com/$l"
+sleep 2
+echo $red "Terimakasih Telah Menggunakan Tools Ini:)"
+sleep 1
+exit
